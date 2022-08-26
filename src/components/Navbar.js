ff431f4633cd2863356216af313d4bae1129a163
Pritam Manna <69286564+Pritam-hrxcoder13@users.noreply.github.com>
2022-08-26 21:02:29 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
     return (
                 <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
                     <div className="container-fluid">
-                    <Link className="navbar-brand" to="/about">{props.title}</Link>
+                    <Link className="navbar-brand" to="/">{props.title}</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -18,7 +18,7 @@ export default function Navbar(props) {
                             <Link className="nav-link" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/">{props.about}</Link>
+                            <Link className="nav-link" to="/about">{props.about}</Link>
                         </li>
                         </ul>
                         <form className="d-flex">
